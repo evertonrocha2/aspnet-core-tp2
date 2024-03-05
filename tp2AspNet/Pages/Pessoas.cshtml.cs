7868b0998a6aba2a76efcac4765909dcf395c9c2
evertonrocha2 <evertondarocha2018@gmail.com>
2024-03-04 21:26:31 -0300

    Adicionar arquivos de projeto.

---
+++ tp2AspNet/Pages/Pessoas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tp2AspNet.Pages
+{
+    public class PessoasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
